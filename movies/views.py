1166f6c8e0c1b07ade801c4f783f10224ee5065b
henriquelopes <henriquelopescavalcante@gmail.com>
2024-09-24 13:18:28 -0300

    alteraçoes feitas nas views de movies

--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,9 @@
-from rest_framework import generics
+from rest_framework import generics, views, response, status
 from rest_framework.permissions import IsAuthenticated
 from app.permissions import GlobalDefaultPermission
 from movies.models import Movie
 from movies.serializers import MovieModelSerializer
+
 
 class MovieCreateListView(generics.ListCreateAPIView):
     permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
@@ -13,3 +14,15 @@ class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Movie.objects.all()
     serializer_class = MovieModelSerializer
+
+
+class MovieStatsView(views.APIView):
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
+    queryset = Movie.objects.all()
+    
+
+    def get(self, request):
+        return response.responses(
+            data ={'message': 'funcionou'},
+            status=status.HTTP_200_OK,
+    )
